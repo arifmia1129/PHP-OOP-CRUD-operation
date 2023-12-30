60702457c458fd947193d73ceb91a3759a0ae94f
mdarifmia <arif@supremesupports.com.au>
2023-12-30 11:42:14 +0600

    insert data to table

--- index.php
+++ index.php
@@ -1,30 +1,54 @@
 <?php
 
-    class db{
-        public $hostname;
-        public $username;
-        public $password;
-        public $db_name;
+class db
+{
+    public $hostname;
+    public $username;
+    public $password;
+    public $db_name;
+
+    public $conn;
 
 
-        public function __construct($hostname, $username, $password, $db_name) {
-            $this->hostname = $hostname;
-            $this->username = $username;
-            $this->password = $password;
-            $this->db_name = $db_name;
 
-            $conn = mysqli_connect($this->hostname,$this->username, $this->password, $this->db_name);
+    // connect to db
+    public function __construct($hostname, $username, $password, $db_name)
+    {
+        $this->hostname = $hostname;
+        $this->username = $username;
+        $this->password = $password;
+        $this->db_name = $db_name;
 
-            if(!$conn) {
-                echo mysqli_error($conn());
-            }else{
-                echo "Database Connected";
-            }
+        $this->conn = mysqli_connect($this->hostname, $this->username, $this->password, $this->db_name);
 
+        if (!$this->conn) {
+            echo "Failed to connect db\n";
+        } else {
+            echo "Database Connected\n";
         }
-
     }
 
-    $database = new db("localhost", "root", "", "learning");
+
+    // insert data to table
+
+    public function insert_info($name, $gender, $class, $roll)
+    {
+        $stmt = $this->conn->prepare("INSERT INTO student(name, gender, class, roll) VALUES(?, ?, ?, ?)");
+
+        $stmt->bind_param("ssss", $name, $gender, $class, $roll);
+
+        if($stmt->execute()) {
+            echo "Data inserted to table successfully\n";
+        }else{
+            echo "Failed to inserted data to table.";
+        }
+
+        $stmt->close();
+    }
+}
+
+$database = new db("localhost", "root", "", "oop");
+
+$database->insert_info("Binu", "female", "Honours", "12345");
 
 ?>
